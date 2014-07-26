41e89c16d1d1befbf50d0a7e1f322375fa898942
Jeswin <jeswinpk@agilehead.com>
2014-07-26 11:19:14 +0530

    yield star can now handle generators and generator functions

--- index.js
+++ index.js
@@ -1,17 +1,48 @@
 (function() {
     "use strict"
 
+    /**
+     * Check if `obj` is a generator.
+     *
+     * @param {Mixed} obj
+     * @return {Boolean}
+     * @api private
+     */
+
+    function isGenerator(obj) {
+      return obj && 'function' == typeof obj.next && 'function' == typeof obj.throw;
+    }
+
+    /**
+     * Check if `obj` is a generator function.
+     *
+     * @param {Mixed} obj
+     * @return {Boolean}
+     * @api private
+     */
+
+    function isGeneratorFunction(obj) {
+      return obj && obj.constructor && 'GeneratorFunction' == obj.constructor.name;
+    }
+
     module.exports = function(debug) {
-      return {
-        parallel: function*(array) {
-          if (debug) {
-            for(var i = 0; i < array.length; i++) {
-              yield* array[i];
+        return {
+            parallel: function*(array) {
+                if (debug) {
+                    var results = [];
+                    for(var i = 0; i < array.length; i++) {
+                        if (isGeneratorFunction(array[i]))
+                            results[i] = yield* array[i]();
+                        else if (isGenerator(array[i]))
+                            results[i] = yield* array[i];
+                        else
+                            throw new Error("Pass an array of generator functions or generators")
+                    }
+                    return results;
+                } else {
+                    return yield array;
+                }
             }
-          } else {
-            yield array;
-          }
         }
-      }
     }
 })();
